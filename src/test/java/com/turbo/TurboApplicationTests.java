d84efba2c28ee74461135f971f346b2cc331639d
Marek Krzyszczyk <marek.krzyszczyk@wp.pl>
2020-01-27 21:05:54 +0100

    index.html

---
+++ src/test/java/com/turbo/TurboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
